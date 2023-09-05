075bc948440b3a5d53c4f982004d2fdde6cedf04
Azuyamat <discordazu@gmail.com>
2023-09-05 19:57:52 -0400

    Initial commit

---
+++ src/main/java/com/azuyamat/notenoughtime/NotEnoughTime.kt
@@ -0,0 +1,12 @@
+package com.azuyamat.notenoughtime
+
+import net.fabricmc.api.ModInitializer
+
+class NotEnoughTime : ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    override fun onInitialize() {
+        print("Sup g")
+    }
+}
---
+++ src/main/java/com/azuyamat/notenoughtime/client/NotEnoughTimeClient.kt
@@ -0,0 +1,10 @@
+package com.azuyamat.notenoughtime.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class NotEnoughTimeClient : ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    override fun onInitializeClient() {}
+}
